0c9ee86c6fcf240c76b8dbbe3058f2241d196bb2
ric taglietti <rictaglietti@gmail.com>
2019-10-10 09:42:39 -0400

    add pill form and date range inputs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const submitButton = document.querySelector('.promo-submit');
 const heroButton = document.querySelector('.hero-banner-button');
 const bannerButton = document.querySelector('.banner-button');
+const pillsButton = document.querySelector('.pills-button');
 const componentMenuButton = document.querySelectorAll('.component-menu-btn');
 
 submitButton.addEventListener('click', (e)=>{
@@ -42,8 +43,8 @@ submitButton.addEventListener('click', (e)=>{
     codeSnippet.innerText += `
     countDown(
         '.${localStorage.getItem('heroParentElement')}',
-        '${localStorage.getItem('countdownStart')}', 
-        '${localStorage.getItem('countdownEnd')}', 
+        '${dateFormat(localStorage.getItem('countdownStart'))}', 
+        '${dateFormat(localStorage.getItem('countdownEnd'))}', 
     {
         offer:     '${localStorage.getItem('bannerOffer')}',
         text:      '${localStorage.getItem('bannerText')}',
@@ -68,7 +69,7 @@ bannerButton.addEventListener('click', ()=> {
 
     // HIDES FORM INPUTS FROM OTHER COMPONENTS
     for(let i = 0; i < oldFormContent.length; i++) {
-        oldFormContent[i].style.visbility = 'invisible';
+        oldFormContent[i].style.display = 'none';
     }
 
     // APPENDS FORM INPUTS FOR BANNER and COUNTDOWN
@@ -144,7 +145,7 @@ heroButton.addEventListener('click', ()=> {
     
     // HIDES FORM INPUTS FROM OTHER COMPONENTS
     for(let i = 0; i < oldFormContent.length; i++) {
-        oldFormContent[i].setAttribute("type", "hidden");
+        oldFormContent[i].style.display = "none";
     }
 
     // APPENDS FORM INPUTS FOR HERO
@@ -237,6 +238,168 @@ heroButton.addEventListener('click', ()=> {
 
 });
 
+// POPULATES PILLS FORM UPON CLICKING HERO BANNER BUTTON
+pillsButton.addEventListener('click', () => {
+    let oldFormContent = document.querySelectorAll('.form-group');
+    let form = document.querySelector('.promo-inputs');
+    
+    // HIDES FORM INPUTS FROM OTHER COMPONENTS
+    for(let i = 0; i < oldFormContent.length; i++) {
+        oldFormContent[i].style.display = "none";
+    }
+    // APPENDS FORM INPUTS FOR BANNER and COUNTDOWN
+    let html = `
+    <div class="d-flex justify-content-between include-component">
+        <h3>Pills</h3>
+        <div>
+            <label for="include-pills">Include Component?</label>
+            <input type="checkbox" id="include-pills" onclick="save(this)">
+        </div>
+    </div>
+    <div class="pills-form-group">
+        <div class="form-group">
+            <label for="pillColor">Pill Color:</label>
+            <input type="text" class="form-control pill-input" id="pillColor" onkeyup="saveValue(this)" placeholder="Pill Color">
+        </div>
+
+        <div class="form-group">
+            <label for="pillText">Pill Text:</label>
+            <input type="text" class="form-control pill-input" id="pillText" placeholder="Pill Text" onkeyup="saveValue(this)">
+        </div>
+
+        <div class="form-group">
+            <label for="pillClass">Pill Class:</label>
+            <input type="text" class="form-control pill-input" id="pillCLass" placeholder="Pill Class" onkeyup="saveValue(this)">
+        </div>
+
+        <div class="form-group">
+            <label for="shipCodes">Ship Codes:</label>
+            <input type="text" class="form-control pill-input" id="shipCodes" onkeyup="saveValue(this)" placeholder="Ships IDs">
+        </div>
+
+        <hr>
+        
+        <div class="form-group">
+            <h3>Promo Dates</h3>
+            <div class="d-flex justify-content-between">
+                <div class="dates-input">
+                    <label for="pillPromoStartDates">Start Date:</label>
+                    <input type="date" class="form-control pill-input" id="pillPromoStartDates" onkeyup="saveValue(this)" placeholder="Pill Promo Start Date">
+                </div>
+                <div class="dates-input">
+                    <label for="pillPromoEndDates">End Date:</label>
+                    <input type="date" class="form-control pill-input" id="pillPromoEndDates" onkeyup="saveValue(this)" placeholder="Pill Promo End Date">
+                </div>
+            </div>
+            <div class="insert-pill-promo-dates">
+            
+            </div>
+
+            <button class="btn add-promo-dates btn-success">+ Add Promo Dates +</button>
+        </div>
+
+        <hr>
+
+        <div class="form-group">
+            <h3>Eligible Sailing Ranges</h3>
+            <div class="d-flex justify-content-between">
+                <div class="dates-input">
+                    <label for="sailingStartDate">Start Date:</label>
+                    <input type="date" class="form-control pill-input" id="sailingStartDate" onkeyup="saveValue(this)" placeholder="Start Date">
+                </div>
+                <div class="dates-input">
+                    <label for="sailingEndDate">End Date:</label>
+                    <input type="date" class="form-control pill-input" id="sailingEndDate" onkeyup="saveValue(this)" placeholder="End Date">
+                </div>
+            </div>
+            <div class="insert-pill-sailing-dates">
+
+            </div>
+            <button class="btn add-sailing-dates btn-success">+ Add Sailing Dates +</button>
+        </div>
+
+        <hr>
+
+        <div class="form-group">
+            <label for="bannerTimerText">Banner Timer Text:</label>
+            <input type="text" class="form-control pill-input" id="bannerTimerText" onkeyup="saveValue(this)" placeholder="Banner Timer Text">
+        </div>
+
+        <div class="form-group">
+            <label for="bannerMarket">Banner Market:</label>
+            <input type="text" class="form-control pill-input" id="bannerMarket" onkeyup="saveValue(this)" placeholder="Countdown Market">
+        </div>
+    </div>
+    `;
+
+    form.innerHTML = html
+    const newFormContent = document.querySelectorAll('.pill-input');
+    const addPromoDatesBtn = document.querySelector('.add-promo-dates')
+    const addSailingDatesBtn = document.querySelector('.add-sailing-dates')
+    let promoClickCount = 1
+    let sailingClickCount = 1
+
+    for (let i = 0; i < newFormContent.length; i++) {
+        newFormContent[i].value = getSavedValue(newFormContent[i].id);
+    }
+
+    addPromoDatesBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        promoClickCount += 1;
+        let insertMorePromoDates = document.querySelector('.insert-pill-promo-dates')
+
+        let promoDateInputs = `
+        <div class="d-flex justify-content-between added-inputs">
+            <div class="dates-input">
+                <label for="pillPromoStartDates${promoClickCount}">Start Date:</label>
+                <input type="date" class="form-control added-promo-date" id="pillPromoStartDates${promoClickCount}" onkeyup="saveValue(this)">
+            </div>
+            <div class="dates-input">
+                <label for="pillPromoEndDates${promoClickCount}">End Date:</label>
+                <input type="date" class="form-control added-promo-date" id="pillPromoEndDates${promoClickCount}" onkeyup="saveValue(this)">
+            </div>
+        </div>
+        `;
+        insertMorePromoDates.innerHTML += promoDateInputs;
+
+        let addedPromoDates = document.querySelectorAll('.added-promo-date');
+
+        for(let i = 0; i < addedPromoDates.length; i++) {
+            addedPromoDates[i].value = getSavedValue(addedPromoDates[i].id);
+        }
+
+    });
+
+    addSailingDatesBtn.addEventListener('click', (e) => {
+        e.preventDefault()
+        sailingClickCount += 1;
+        let insertMoreSailingDates = document.querySelector('.insert-pill-sailing-dates')
+
+        let sailingDateInputs = `
+        <div class="d-flex justify-content-between added-inputs">
+            <div class="dates-input">
+                <label for="sailingStartDate${sailingClickCount}">Start Date:</label>
+                <input type="date" class="form-control added-sailing-date" id="sailingStartDate${sailingClickCount}" onkeyup="saveValue(this)">
+            </div>
+            <div class="dates-input">
+                <label for="sailingEndDate${sailingClickCount}">End Date:</label>
+                <input type="date" class="form-control added-sailing-date" id="sailingEndDate${sailingClickCount}" onkeyup="saveValue(this)">
+            </div>
+        </div>
+        `;
+
+        insertMoreSailingDates.innerHTML += sailingDateInputs;
+
+        const addedSailingDates = document.querySelectorAll('.added-sailing-date');
+        
+        for (let i = 0; i < addedSailingDates.length; i++) {
+            addedSailingDates[i].value = getSavedValue(addedSailingDates[i].id);
+        }
+
+    })
+    
+})
+
 componentMenuButton.forEach(item => {
     item.addEventListener('click', () => {
         submitButton.style.display = "block";
@@ -245,7 +408,6 @@ componentMenuButton.forEach(item => {
 
 // SAVE COMPONENT CHECKBOX
 function save(e) {
-    
     let checkbox = document.querySelector(`#${e.id}`).checked
     localStorage.setItem(e.id, checkbox)
 }
@@ -280,5 +442,22 @@ function copyText() {
     document.execCommand("copy");
     textArea.remove()
 } 
+
+function dateFormat(date) {
+    let inputDate = date
+    let year, month, day, time;
+
+    let arr1 = inputDate.split('-');
+    year = arr1[0];
+    month = arr1[1];
+
+    let arr2 = arr1[2].split('T');
+    day = arr2[0];
+    time = arr2[1];
+
+    let monthsArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    
+    return `${monthsArr[month-1]} ${day} ${year} ${time + ':00'}`
+}
 
 
